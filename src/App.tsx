dc98c1ca18942ba0b543cc14d2a18e6d5a643ade
refrain62 <nao_h13@hotmail.com>
2024-11-21 14:25:43 +0900

    Button要素のイベント

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = (event: any) => {
+    console.log(event);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
