ce9d93246e6148459bb02a5986801f4f5fff3c62
refrain62 <nao_h13@hotmail.com>
2024-11-21 14:26:37 +0900

    コメント

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // any など型を付けないとエラーになる
+  // 「TS7006: Parameter 'event' implicitly has an 'any' type.」
   const handleClick = (event: any) => {
     console.log(event);
   };
