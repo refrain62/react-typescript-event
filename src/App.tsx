043367008484e6d11570ad5e425f81f45effb37a
refrain62 <nao_h13@hotmail.com>
2024-11-21 15:27:27 +0900

    MouseEventの設定

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import './App.css';
 function App() {
   // any など型を付けないとエラーになる
   // 「TS7006: Parameter 'event' implicitly has an 'any' type.」
-  const handleClick = (event: any) => {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     console.log(event);
   };
 
+  // event にカーソルを充てると 「(parameter) event: React.MouseEvent<HTMLButtonElement, MouseEvent>」と型の情報をしてくれる
   return (
     <div className="App">
       <button onClick={handleClick}>Click</button>
+      <button onClick={(event)=>{}}>Click</button>
     </div>
   );
 }
