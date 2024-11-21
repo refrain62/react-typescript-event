4656457cec6b50601aec8dd5ec02df79abcdce84
refrain62 <nao_h13@hotmail.com>
2024-11-21 16:05:48 +0900

    input要素からのイベント

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+
+  // input要素のイベント
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
+
+
   // any など型を付けないとエラーになる
   // 「TS7006: Parameter 'event' implicitly has an 'any' type.」
   const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -40,6 +48,14 @@ function App() {
       <button onClick={handleBtnDivUnionClick}>Union Click</button>
       <div onClick={handleBtnDivUnionClick}>Union Click</div>
       
+      {/* input要素 */}
+      <input value={name} onChange={handleChange} />
+      <input value={name} onChange={(event) => {}} />
+      <div>name: {name}</div>
+
+      {/* インライン記述による型推論 */}
+      <textarea value={name} onChange={(event) => setName(event.target.value)} />
+      <div>name: {name}</div>
     </div>
   );
 }
